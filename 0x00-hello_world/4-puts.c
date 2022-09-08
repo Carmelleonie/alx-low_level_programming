c533b65330542ebebd5316abb6f19aff065268b1
Carmelleonie <sagboleonie7@gmail.com>
2022-09-08 09:59:10 -0700

    My 4 commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * use the function puts 
+ * this programme will print "Programming is like building a multilingual puzzle
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
