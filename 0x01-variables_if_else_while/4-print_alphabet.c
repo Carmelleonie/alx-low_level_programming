0cabb08a848eb0be2283eca5e72e32c081aa98b8
Carmelleonie <sagboleonie7@gmail.com>
2022-09-14 11:19:08 +0100

    alpha

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*
+*Desciption: using the main function
+*This program prints the alphabet in lowercase
+*Return: 0 Always (success)
+*/
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+	if (i != 'e' && i != 'q')
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
