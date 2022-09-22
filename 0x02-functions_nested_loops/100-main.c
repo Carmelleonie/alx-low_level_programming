219bd3aeff382a23afdaae352b4f53d56b54a093
Carmelleonie <sagboleonie7@gmail.com>
2022-09-23 00:36:47 +0100

    100 times table

---
+++ 0x02-functions_nested_loops/100-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*main - Entry point
+*
+*Description: using the main function
+*This program prints the time tables
+*Return: 0 Always (success)
+*/
+int main(void)
+{
+	print_times_table(3);
+	_putchar('\n');
+	print_times_table(5);
+	_putchar('\n');
+	print_times_table(98);
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+*print_times_table - prints the times table with
+*parameter
+*@n: parameter
+*Return: returns nothing
+*/
+void print_times_table(int n)
+{
+	int i, j, m;
+
+	if (n <= 15 && n >= 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('0');
+
+			for (j = 1; j <= n; j++)
+			{
+				_putchar(',');
+				_putchar(' ');
+				m = i * j;
+				if (m <= 99)
+				{
+					_putchar(' ');
+				}
+				if (m <= 9)
+				{
+					_putchar(' ');
+				}
+				if (m >= 100)
+				{
+					_putchar((m / 100) + '0');
+					_putchar((m / 10) % 10 + '0');
+				}
+				else if (m <= 99 && m >= 10)
+				{
+					_putchar((m / 10) + '0');
+				}
+				_putchar((m % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
+}
