3b507a283931f1153a6dc5b7160cc749d146533e
Carmelleonie <sagboleonie7@gmail.com>
2022-09-22 18:38:23 +0100

    8-24 hours

---
+++ 0x02-functions_nested_loops/8-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+*main - Entry point
+*
+*Description: using the main function
+*This program prints hours
+*Return: ) Always (success)
+*/
+int main(void)
+{
+	jack_bauer();
+	return (0);
+}
