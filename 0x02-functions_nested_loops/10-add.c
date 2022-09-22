727e73f625acf6faba594ce76722d71e652e138b
Carmelleonie <sagboleonie7@gmail.com>
2022-09-22 19:01:27 +0100

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+*add - Entry point
+*
+*@a: number 1
+*@b: number 2
+*This program adds two integers and returns the result
+*Return: a + b
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*main- Entry point
+*
+*Description: using the main function
+*This program print the sum of numbers
+*Return (0)
+*/
+int main(void)
+{
+	int i;
+
+	i = add(4, 8);
+	printf("%d", i);
+	return (0);
+}
