168b6d3742406b9f1756de6d2fedb7bd1eb73b2c
Carmelleonie <sagboleonie7@gmail.com>
2022-09-22 22:53:30 +0100

    print to 98

---
+++ 0x02-functions_nested_loops/11-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*main - Entry point
+*
+*Description: using the main function
+*This program prints numbers
+*Return: 0 Always (success)
+*/
+int main(void)
+{
+	print_to_98(0);
+	print_to_98(98);
+	print_to_98(111);
+	print_to_98(81);
+	print_to_98(-10);
+    	return (0);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*print_to_98 - Entry point
+*
+*@n: print to 98
+*This program prints prints the 9 times table, starting with 0
+*Return: 0 Always (success)
+*/
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n <= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n++;
+		}
+	}
+	else if (n > 98)
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n--;
+		}
+	}
+	else
+	{
+		printf("98");
+	}
+	printf("\n");
+}
