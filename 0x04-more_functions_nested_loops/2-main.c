0e2a5f7c460702c059e385c3762da045e43c572a
Carmelleonie <sagboleonie7@gmail.com>
2022-09-23 02:25:55 +0100

    mul

---
+++ 0x04-more_functions_nested_loops/2-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+*main -Entry point
+*Description: using the main function
+*
+*Return: 0 Always
+*/
+int main(void)
+{
+	printf("%d\n", mul(12, 15));
+	printf("%d\n", mul(2, 12));
+	return (0);
+}
