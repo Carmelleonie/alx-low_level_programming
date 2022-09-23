e46e2fbe5f43746e93dbe09a3f72dfdd8920570e
Carmelleonie <sagboleonie7@gmail.com>
2022-09-23 19:57:39 +0100

    puts

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+*puts_half -prints a string
+*@str: string to print
+*
+*Return: nothing
+*/
+void puts_half(char *str)
+{
+	int i, j, len;
+
+	len = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		len++;
+	}
+
+	j = (len / 2);
+	if ((len % 2) == 1)
+	{
+		j = ((len + 1) / 2);
+	}
+	for (i = j; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
