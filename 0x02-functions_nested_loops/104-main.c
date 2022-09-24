f9dfe328c2494101d89391f2a441c5b1259bca16
Carmelleonie <sagboleonie7@gmail.com>
2022-09-24 17:40:28 +0100

    fib

---
+++ 0x02-functions_nested_loops/104-main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*main - Entry point
+*
+*
+*Return: 0 Always
+*/
+int main(void)
+{
+	int a, b, c;
+
+	a = 12;
+	b = 5;
+	c = a + b;
+
+	printf("%d", c);
+
+	return (0);
+}
