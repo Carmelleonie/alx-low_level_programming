b42decf4cf6b41b6c27c4f6e323a2976dbeb296f
Carmelleonie <sagboleonie7@gmail.com>
2022-09-24 20:35:01 +0100

    102 fibo

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+*main - prints out first 50
+*fibonacci suit numbers
+*Return: 0 Always
+*/
+int main(void)
+{
+	int i;
+	unsigned long a = 0, b = 1, c;
+
+	for (i = 0; i < 50; i++)
+	{
+		c = a + b;
+		printf("%lu", c);
+		a = b;
+		b = c;
+
+		if (i == 49)
+		{
+			printf("\n");
+		}
+		else
+		{
+			printf(",");
+		}
+	}
+	return (0);
+}
