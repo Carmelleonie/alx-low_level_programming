040f945ebb11a13281c0279d96ae46707e5132eb
Carmelleonie <sagboleonie7@gmail.com>
2024-02-02 01:49:35 +0100

    print first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+*print_first - it prints a given sentence the sentence in the main function
+*Return: Nothing
+*/
+void print_first()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
