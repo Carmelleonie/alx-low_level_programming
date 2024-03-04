742898f7dd7db28a54264d8e92fab6d84686c0d2
Carmelleonie <sagboleonie7@gmail.com>
2024-03-04 19:14:32 +0100

    change

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
-*main - enter point
+* main - main function in the task
 *
-*Description: using the main function
-*return: 1
+*
+*Return: return 1
 */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-	sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,25 +1,17 @@
 #include <stdio.h>
-
 /**
-*main - Entry point
+*main - main function
+*Description : a C program that prints the size of various
+*types on the computer it is compiled and run on.
 *
-*Description: using the main function
-*This function prints the size of various types of computer
-*
-*Return: 0 Always (success)
+*Return: return 0
 */
 int main(void)
 {
-	char c;
-	int a;
-	long int b;
-	long long int x;
-	float f;
-
-	printf("Size of a char: %d byte(s)\n", c);
-	printf("Size of an int: %d byte(s)\n", a);
-	printf("Size of a long int: %d byte(s)\n", b);
-	printf("Size of a long long int: %d byte(s)\n", x);
-	printf("Size of a float: %d byte(s)\n", f);
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
