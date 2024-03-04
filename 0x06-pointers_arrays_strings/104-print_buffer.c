a1739bd734b615992ee327b8ad3dbc154054af00
Carmelleonie <sagboleonie7@gmail.com>
2024-03-04 18:33:06 +0100

    add

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,17 +1,55 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ *print_buffer -  C function that prints the content of an
+ *  inputted number of bytes from a buffer.
+ * Prints 10 bytes per line.
+ * Starts with the position of the first byte in hexadecimal (8 chars),
+ * starting with `0`.
+ * Each line shows the hexadecimal content (2 chars) of the buffer,
+ * 2 bytes at a time, separated by a space.
+ * Each line shows the content of the buffer.
+ * Prints the byte if it is printable; if not, prints `.`.
+ * Each line ends with a new line `\n`.
+ * If the inputted byte size is 0 or less, the function only prints a new line.
+ * @b: number of bytes
+ * @size: size of the byte
  */
-int main(void)
+void print_buffer(char *b, int size)
 {
-    char buffer[] = "This is a string!\0And this is the rest of the #buffer :)\1\2\3\4\5\6\7#cisfun\n\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x20\x21\x34\x56#pointersarefun #infernumisfun\n";
+	int i = 0, j;
 
-    printf("%s\n", buffer);
-    printf("---------------------------------\n");
-    print_buffer(buffer, sizeof(buffer));
-    return (0);
+	if (size < 0)
+	{
+		printf("\n");
+		return;
+	}
+
+	while (i < size)
+	{
+		if (i % 10 == 0)
+			printf("%08x: ", i);
+		for (j = i; j < i + 9; j += 2)
+		{
+			if ((j < size) && ((j + 1) < size))
+				printf("%02x%02x: ", b[j], b[j + 1]);
+			else
+			{
+				while (++j <= i + 10)
+				{
+					printf(" ");
+				}
+				printf("	");
+			}
+		}
+		for (j = i; j <= i + 9 && j < size; j++)
+		{
+			if (b[j] >= 32 && b[j] <= 126)
+				printf("%c", b[j]);
+			else
+				printf(".");
+		}
+		printf("\n");
+		i += 10;
+	}
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -15,7 +15,9 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 char *leet(char *str);
-char *add_strings(char *n1, char *n2, char *r, int r_index);
+int _atoi(char *s);
+void int_to_string(int n);
+char transform_2(char x);
 
 
 #endif
