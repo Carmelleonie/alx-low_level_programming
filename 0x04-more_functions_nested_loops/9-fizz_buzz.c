33ef74be6626adeac880d433d81221d3cf7a1bab
Carmelleonie <sagboleonie7@gmail.com>
2024-03-04 18:54:45 +0100

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,35 +9,31 @@
 */
 int main(void)
 {
-	int num;
+	int num = 1;
 
-	num = 0;
-	while (num++ < 100)
+	while (num < 100)
 	{
 		if ((num % 3 == 0) && (num % 5 == 0))
 		{
-			printf("FizzBuzz");
+			printf("FizzBuzz ");
 		}
-		else if ((num % 3) == 0 && (num % 5) != 0)
+		else if ((num % 3) == 0)
 		{
-			printf("Fizz");
+			printf("Fizz ");
 		}
 		else if ((num % 5) == 0)
 		{
 			if (num != 100)
-			{
-				printf("Buzz");
-			}
+				printf("Buzz ");
+
 			else
-			{
 				printf("Buzz");
-			}
 		}
 		else
 		{
-			printf("%d", num);
+			printf("%d ", num);
 		}
-		printf(" ");
+		num++;
 	}
 	printf("\n");
 	return (0);
